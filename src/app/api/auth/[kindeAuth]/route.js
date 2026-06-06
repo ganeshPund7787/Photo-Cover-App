6bb4664d221ac98178ab0b5de983664641ae3e61
ganeshpund7787 <ganupund7787@gmail.com>
2026-06-06 17:00:47 +0530

    Fix Kinde Auth Build Bug

--- src/app/api/auth/[kindeAuth]/route.js
+++ src/app/api/auth/[kindeAuth]/route.js
@@ -1,3 +1,5 @@
+
+export const dynamic = 'force-dynamic'
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 export const GET = handleAuth();
 console.log("This is from route file : ", GET);
